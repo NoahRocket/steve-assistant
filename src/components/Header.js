a76b9b9cbf795e93dc3378acd3ab33cbe4e10219
NoahRocket <47186075+NoahRocket@users.noreply.github.com>
2024-12-14 14:55:53 +0100

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="flex justify-end p-4 bg-blue-500 text-white">
+      <h1 className="text-2xl font-bold">Steve</h1>
+    </header>
+  );
+};
+
+export default Header;
